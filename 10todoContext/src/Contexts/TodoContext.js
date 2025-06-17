681bdf64d8a05099a134bf8f406aad33671d7105
Themiya <themiyakaluarachchi@gmail.com>
2025-06-17 21:54:51 +0530

    init

---
+++ 10todoContext/src/Contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            text: "Todo Message",
+            completed: false
+        },
+        
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleCompleted: (id) => {}
+
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
