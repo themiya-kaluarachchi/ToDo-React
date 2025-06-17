30e1884cf00884fdcfe029bfb2e4846fcdbb922c
Themiya <themiyakaluarachchi@gmail.com>
2025-06-17 22:33:49 +0530

    ..

--- 10todoContext/src/App.jsx
+++ 10todoContext/src/App.jsx
@@ -2,17 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './Contexts/TodoContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [ {id: Date.now(), ...todo} , ...prev ])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => 
+      prev.map((prevTodo) => 
+        prevTodo.id === id ? {...prevTodo, completed:
+          !prevTodo.completed} : prevTodo
+        )
+      )
+  }
 
   return (
-    <>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    </>
+    </TodoProvider>
   )
+  
 }
 
 export default App
